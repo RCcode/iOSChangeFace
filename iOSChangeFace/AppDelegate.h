4075f2b1e56e2491aadf12aaef74635323dc08d1
gaoluyangrc <rcplatform@rcplatforms-Mac-mini.local>
2014-07-26 15:06:30 +0800

    Initial Commit

---
+++ iOSChangeFace/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSChangeFace
+//
+//  Created by gaoluyangrc on 14-7-26.
+//  Copyright (c) 2014年 rcplatform. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
